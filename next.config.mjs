20017dfb6c17de339526e69ac7162a9a102ae0bd
jrfs22 <mr.jrfs22@gmail.com>
2025-07-21 20:50:07 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ import nextI18NextConfig from './next-i18next.config.js'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ],
   eslint: {
     ignoreDuringBuilds: true,
   },
